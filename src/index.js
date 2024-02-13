824e464294d78a8d1117ad91fbc7a41aa02e3f0a
Prateek-5 <prateek.ku11@gmail.com>
2024-02-13 21:15:35 +0530

    Added the tweets and hastag repo and service layer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ const  connect  = require('./config/database');
 const app=express();
 
 
-const TweetRepository=require('../src/repository/tweet-repository')
+const TweetService=require('./services/tweet-service');
+
 
 app.listen(3500,async ()=>{
     console.log(`The server is running on PORT ${3500}`);
     await connect();
     console.log("MongoDB connected");
     
-
+    const service=new TweetService();
+    const response=await service.create({
+        content:"This is my first tweet really #exited about the same #wow",
+    })
+    console.log(response);
 
 
 })  
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,46 +1,51 @@
 const Tweet = require('../models/tweet');
-const tweet=require('../models/tweet');
 
-
-class TweetRepository{
-    async create(data){
+class TweetRespository {
+    
+    async create(data) {
         try {
-            const tweet =await Tweet.create(data);
+            const tweet = await Tweet.create(data);
             return tweet;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async get(id){
+    async get(id) {
         try {
-            const tweet =await Tweet.findById(id);
-            return tweet;
-
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    async update(tweetId,data){
-        try {
-            const tweet =await Tweet.findByIdAndUpdate(tweetId,data);
+            const tweet = await Tweet.findById(id);
             return tweet;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async destroy(id){
+    async getWithComments(id) {
         try {
-            const tweet =await Tweet.findByIdAndDelete(id);
+            const tweet = await Tweet.findById(id).populate({path: 'comments'}).lean();
             return tweet;
-
         } catch (error) {
             console.log(error);
+        }
+    }
 
+    async destroy(id) {
+        try {
+            const tweet = await Tweet.findByIdAndRemove(id);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAll(offset, limit) {
+        try {
+            const tweet = await Tweet.find().skip(offset).limit(limit);
+            return tweet;
+        } catch (error) {
+            console.log(error);
         }
     }
 }
 
-module.exports=TweetRepository;
+module.exports = TweetRespository;
